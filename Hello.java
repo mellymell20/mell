8fccee2d5d6986112b221ee7c40c82febba671a9
mellymell20 <69982640+mellymell20@users.noreply.github.com>
2020-08-20 13:05:24 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,17 @@
+/**************************************
+*Hello.java
+***************************************
+*Melissa Clottey
+*08/20/2020
+*CMSC225 SEC001
+*****************************************/
+public class Hello{
+public static void main (String[] args) {
+System.out.println("<Melissa Clottey>");
+System.out.println("<Hello.java>");
+System.out.println("<Lab One>");
+System.out.println("<08/20/2020>");
+System.out.println("<CMSC225 SEC001>");
+System.out.println();
+}
+}
